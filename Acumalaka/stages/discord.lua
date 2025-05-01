3952b7924b0cd3e67c35937a182b1b08cd9c43fe
iLusion <40916277+iLusion3431@users.noreply.github.com>
2025-05-01 22:37:27 +0700

    Add files via upload

---
+++ Acumalaka/stages/discord.lua
@@ -0,0 +1,9 @@
+function onCreate()
+
+        makeLuaSprite('BG', 'discord', -900, -700);
+        scaleObject('BG', 10, 10); 
+        addLuaSprite('BG', false);
+	setScrollFactor('BG', 0.7, 0.7);
+       close(true);
+
+end
---
+++ Acumalaka/stages/malaka.lua
@@ -0,0 +1,9 @@
+function onCreate()
+
+        makeLuaSprite('BG', 'backmalaka', -820, -400);
+        scaleObject('BG', 1.8, 1.8); 
+        addLuaSprite('BG', false);
+	setScrollFactor('BG', 0.7, 0.7);
+       close(true);
+
+end
